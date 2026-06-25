e5c83f41dbb7f2515c1142f4e78629fba857b913
shivam <shvam12340987@gmail.com>
2026-06-25 14:36:28 +0530

    polymorphism

---
+++ 13-polymorphism.py
@@ -0,0 +1,15 @@
+# poly means many-forms 
+
+class A:
+    def greet(self):
+        print("Hello from A")
+        
+class B:
+    def greet(self):
+        print("Hello from B")
+        
+obj1 = A()
+obj2 = B()
+
+obj1.greet()
+obj2.greet()
