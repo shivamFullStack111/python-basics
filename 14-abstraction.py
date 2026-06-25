57278d34968eaf15ef92de76e17c7dacd39bcad7
shivam <shvam12340987@gmail.com>
2026-06-25 15:47:49 +0530

    abstraction

---
+++ 14-abstraction.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+class Rules(ABC):
+    
+    @abstractmethod
+    def greet(self):
+        pass 
+    
+class India(Rules):
+    
+    def greet(self):                        # if we cannot create this mehtod and create object of this class then interpreter will throw error  (TypeError: Can't instantiate abstract class India without an implementation for abstract method 'greet')
+        print('Hello India!') 
+        
+
+obj = India()
+obj.greet()
+
+
+
+
+# Abstraction means define a absteaction method or attribute in a class 
+# and the other child class who will inherit abstract class also have to
+# define the same method and attributes that defined in abstraction class
