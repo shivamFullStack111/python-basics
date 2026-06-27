3910ba93b6cbf57d98cc17a2f24dc035c4533961
shivam <shvam12340987@gmail.com>
2026-06-27 15:14:23 +0530

    lambda function

---
+++ 20-lambda-function.py
@@ -0,0 +1,9 @@
+#  Lambda Function is anonymous (naam ke bina) function, which use for small task in single line. 
+
+# SYNTAX: 
+# lambda arguments: expression 
+
+
+addition = lambda a,b: a+b 
+
+print(addition(5,6))
